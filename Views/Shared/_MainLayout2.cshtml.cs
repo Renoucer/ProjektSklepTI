4b967bc17a7cb21def221a2fe36d689867091f15
112 <112@112-15>
2025-03-03 15:27:59 +0100

    Dodano szablon bootstrap

---
+++ Views/Shared/_MainLayout2.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjektSklepTI.Views.Shared
+{
+    public class _MainLayout2Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
