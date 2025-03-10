ba8a46f6bd5b6257dd59dd129a2516fe3f78505f
112 <112@112-15>
2025-03-10 16:27:50 +0100

    naprawiono bugi

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "StaticSites",
     pattern:"Info/{siteName}",
-    defaults: "{controller=Home}/{action=Index}/{id?}"
+    defaults: new {controller="Home", action="FooterSites"}
     );
 
 app.MapControllerRoute(
