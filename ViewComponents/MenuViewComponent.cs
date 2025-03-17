24de282620bd9c781613b03885ec0dab9993f17a
112 <112@112-15>
2025-03-17 15:52:35 +0100

    git

---
+++ ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjektSklepTI.DAL;
+
+namespace ProjektSklepTI.ViewComponents
+{
+    public class MenuViewComponent: ViewComponent
+    {
+        FilmsContext db;
+
+        public MenuViewComponent(FilmsContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var categories=db.Categories.ToList();
+            return await Task.FromResult(View("_Menu", categories));
+        }
+    }
+}
