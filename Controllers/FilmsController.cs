488a66ec6ef25e17579ba4d239ede19c64c37edc
112 <112@112-15>
2025-04-28 16:27:37 +0200

    nowe funkcjonalnosci

--- Controllers/FilmsController.cs
+++ Controllers/FilmsController.cs
@@ -1,15 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ProjektSklepTI.DAL;
-
+using Microsoft.AspNetCore.Mvc;
+using ProjektSklepTI.Models;
+using Azure.Core;
+using Microsoft.AspNetCore.Mvc;
 namespace ProjektSklepTI.Controllers
 {
     public class FilmsController : Controller
     {
         FilmsContext db;
-        public FilmsController(FilmsContext db)
+        IWebHostEnvironment webHost;
+
+        public FilmsController(FilmsContext db, IWebHostEnvironment webHost)
         {
             this.db = db;
+            this.webHost = webHost;
         }
 
         public IActionResult ProductList(string categoryName)
@@ -29,10 +36,66 @@ namespace ProjektSklepTI.Controllers
             var film=  db.Films.Find(filmId);
             return View(film);
         }
-        public IActionResult All()
+        [HttpGet]
+        public IActionResult AddFilm()
         {
-            var films = db.Films.ToList();  // Pobiera wszystkie filmy z bazy
-            return View(films);  // Przekazuje do widoku "all.cshtml"
+            AddViewModel model = new AddViewModel();
+            model.AllCategories = db.Categories.ToList();
+            return View(model);
         }
+        [HttpPost]
+        public IActionResult AddFilm(AddViewModel model)
+        {
+            var posterFolderPath = Path.Combine(webHost.WebRootPath, "posters");
+            var uniquePosterName = model.Poster.FileName + "_" + Guid.NewGuid() + Path.GetExtension(model.Poster.FileName);
+            var filePath = Path.Combine(posterFolderPath, uniquePosterName);
+            model.Poster.CopyTo(new FileStream(filePath, FileMode.Create));
+            model.NewFilm.Poster = uniquePosterName;
+            db.Films.Add(model.NewFilm);
+            db.SaveChanges();
+            return RedirectToAction("AddFilm");
+        }
+        public IActionResult Images()
+        {
+            var films = db.Films.ToList();
+            return View(films);
+        }
+
+        public IActionResult Search(string tekst)
+        {
+            var filmy = from f in db.Films select f;
+            ViewBag.Frza = tekst;
+            if (!String.IsNullOrEmpty(tekst))
+            {
+                filmy=filmy.Where(f => f.Title.ToUpper().Contains(tekst.ToUpper()));
+                return View(filmy.ToList());
+            }
+            return RedirectToAction("Index", "Home");
+        }
+        [HttpGet]
+        public IActionResult EditFilm(int id)
+        {
+           var film = db.Films.Find(id);
+           return View(film);
+
+        }
+        [HttpPost]
+        public IActionResult EditFilm(Film film)
+        {
+            var filmDB = db.Films.Find(film.FilmId);
+            if (filmDB != null)
+            {
+                filmDB.Title = film.Title;
+                filmDB.Director = film.Director;
+                filmDB.Price = film.Price;
+                filmDB.Desc = film.Desc;
+
+                db.Entry(filmDB).State=EntityState.Modified;
+                db.SaveChanges();
+            }
+            return RedirectToAction("Details", new {filmId=filmDB.FilmId});
+
+        }
+
     }
 }
---
+++ Models/AddViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjektSklepTI.Models
+{
+        public class AddViewModel
+        {
+            public Film NewFilm { get; set; }
+            public IFormFile Poster { get; set; }
+            public List<Category> AllCategories { get; set; }
+        }
+    
+}
