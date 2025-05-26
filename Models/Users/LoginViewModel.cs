c7e68f70997773c01260ac637a115ae6da4e6aa7
112 <112@112-15>
2025-05-26 16:19:25 +0200

    still nie bangla

---
+++ Models/Users/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjektSklepTI.Models.Users
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage ="Nie wpisano loginu")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Nie wpisano hasla")]
+        public string Password { get; set; }
+    }
+}
